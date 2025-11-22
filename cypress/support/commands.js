550eefd10d7af12d885cc4fc0db4923db91cce76
Noah Vastola <noahvastola@gmail.com>
2025-11-22 17:46:21 -0600

    Add Cypress support files for smoke tests

---
+++ cypress/support/commands.js
@@ -0,0 +1,10 @@
+// ***********************************************
+// Custom commands for your Cypress tests
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+
+// Example custom command:
+// Cypress.Commands.add('login', (email, password) => { ... })
